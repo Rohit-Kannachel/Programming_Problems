f8dcfb6acd38ed21e090e086372ec20515a3ac43
marcusvel28 <marcus.velazquez@stonybrook.edu>
2024-08-09 13:48:35 -0400

    The request is done

--- problem_set/Q1_Errors.py
+++ problem_set/Q1_Errors.py
@@ -1,5 +1,5 @@
 # This python code has errors, fix them
-#I am working on it
+#I am Done
 from math import pi
 
 
@@ -8,38 +8,38 @@ def counting_numbers(x: int) -> list:
     Creates a list of numbers up to and including x beginning from 0,
     x > 0
     """
-
-    if x < 0:
+    result=[]
+    numbers=0
+    if x <= 0:
         return [0]
-    
-    while x > 0:
-        list.append(x)
+    while numbers <= x:
+        result.append(numbers)
+        numbers += 1
+    return result
 
-    return list
-
-#numbers = counting_numbers(5)
-
-#print(numbers)
+# numbers=counting_numbers(-5)
+# print(numbers)
 
 def old_enough_to_drive(age: str) -> int:
     """
     return True if age is over 16
     """
+    if age>16:
+        return True
+    else:
+        return False
 
-    return age + 16
+# old_enough = old_enough_to_drive(20)
 
-
-# old_enough = old_enough_to_drive(19)
-
-# print(old_enough)
+# print(f"This age is old enough: {old_enough}")
 
 def volume_of_sphere(radius: float) -> float:
     """
     Returns the volume of a sphere given a radius
     """
-    volume = pi*radius**2
+    Volume = (4/3)*pi*radius**3
 
-    return volume
+    return Volume
 
 
 # vol1 = volume_of_sphere(2)
