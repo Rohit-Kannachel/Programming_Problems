2e6a6f935b38a317e008d219ba3d58da1b57ef72
marcusvel28 <marcus.velazquez@stonybrook.edu>
2024-08-12 15:15:49 -0400

    final

--- problem_set/Q2_Chinese_Zodiac.py
+++ problem_set/Q2_Chinese_Zodiac.py
@@ -17,15 +17,18 @@ chinese_dict= {
 year=2000
 def chinese_new_year(x):
     x = int(x)
-    year = (x-2000) % 60
+    year = (x-2024) % 60
     result=[]
     for element in chinese_dict["elements"]:
         for animal in chinese_dict["animals"]:
             order = f"{element} {animal}"
             result.append(order)
     Zodiac = result[year]
-    Messege= f"{x} is the year of the {Zodiac}"
+    if x<2024:
+        Messege = f"{x} was the year of the {Zodiac}"
+    else:
+        Messege = f"{x} is the year of the {Zodiac}"
     return Messege
 
-Zodiac = chinese_new_year(2000)
+Zodiac = chinese_new_year(input("Input a year to determine it's associated Zodiac: "))
 print(Zodiac)
